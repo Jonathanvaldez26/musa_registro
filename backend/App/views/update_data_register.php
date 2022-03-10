bba0e04ca3de23cad3f16f1a8387a3d45336d523
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-03-09 18:20:12 -0600

    Registro - Campo Requerido

--- backend/App/views/update_data_register.php
+++ backend/App/views/update_data_register.php
@@ -86,7 +86,7 @@ echo $header;
                                             </div>
                                         </div>
                                         <div class="col-lg-3 col-12">
-                                            <label class="form-label mt-4">Número de Telefono</label>
+                                            <label class="form-label mt-4">Número de Teléfono</label>
                                             <div class="input-group">
                                                 <input id="telefono" name="telefono" maxlength="10" pattern="[0-9]" class="form-control" type="number" placeholder="+40 735 631 620" onfocus="focused(this)" onfocusout="defocused(this)" value="<?= $userData['telefono'] ?>">
                                             </div>
@@ -102,8 +102,8 @@ echo $header;
                                     <div class="row">
                                         
                                         <div class="col-md-6">
-                                            <label class="form-label mt-4">Restricciones alimenticias</label>
-                                            <input class="form-control" id="alergias" maxlength="149" name="alergias" data-color="dark" type="text" value="<?= $userData['alergias'] ?>" placeholder="Enter something" />
+                                            <label class="form-label mt-4">Restricciones alimenticias *</label>
+                                            <input class="form-control" id="alergias" maxlength="149" required name="alergias" data-color="dark" type="text" value="<?= $userData['alergias'] ?>" placeholder="Enter something" />
                                         </div>
                                         <br>
                                         <br>
