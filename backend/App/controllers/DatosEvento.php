643a70f5fafbfdc1d55607d234ea073dbe688b3f
Gerson Velasco <gvelassco@gmail.com>
2022-03-17 17:41:38 -0600

    17032022

--- backend/App/controllers/DatosEvento.php
+++ backend/App/controllers/DatosEvento.php
@@ -3,12 +3,6 @@ namespace App\controllers;
 defined("APPPATH") OR die("Access denied");
 
 use \Core\View;
-<<<<<<< HEAD
-use \Core\MasterDom;
-use \App\controllers\Contenedor;
-use \Core\Controller;
-=======
->>>>>>> e391cc7967c35833c1dd01f28ccd76a0bd33c5d1
 
 class DatosEvento{
 
