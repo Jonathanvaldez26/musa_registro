a59aefecdce5070198c3ed1317ae2e2486c38e0a
Gerson Velasco <gvelassco@gmail.com>
2022-04-13 11:42:47 -0500

    hola

--- backend/App/controllers/Mailer.php
+++ backend/App/controllers/Mailer.php
@@ -27,10 +27,8 @@ class Mailer
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            // $mail->Username   = 'mujersalud2022@gmail.com';                     //SMTP username
-            // $mail->Password   = 'grupolahe664';                               //SMTP password
-            $mail->Username   = 'contacto@convencionasofarma2022.mx';                     //SMTP username contacto@convencionasofarma2022.mx
-            $mail->Password   = 'lxwqdkznaznpwpcg';  
+            $mail->Username   = 'mujersalud2022@gmail.com';                     //SMTP username
+            $mail->Password   = 'grupolahe664';                               //SMTP password
             $mail->SMTPSecure = 'ssl';
             $mail->SMTPAutoTLS = false;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
