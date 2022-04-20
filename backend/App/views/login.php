fb3e9e9563f6a5cc643de32c768073155252acb6
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-04-20 18:00:32 -0500

    adium 3

--- backend/App/views/login.php
+++ backend/App/views/login.php
@@ -8,7 +8,7 @@ echo $header;
 
             <nav class="navbar navbar-expand-lg  blur blur-rounded top-0  z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
                 <div class="container-fluid">
-                    <img src="/img/adium.png" height="30" alt=""> &nbsp;&nbsp;&nbsp;
+                    <img src="/img/adium.png" height="40" alt=""> &nbsp;&nbsp;&nbsp;
                     <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 ">
                         Mujer Salud 2022
                     </a>
